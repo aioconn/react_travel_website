9cb8025c02952c048da2abd2b4a0ba96a83d243b
aioconn <76408653+aioconn@users.noreply.github.com>
2021-03-17 13:05:32 -0400

    Add github pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Navbar from "./components/Navbar";
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, HashRouter, Switch, Route } from "react-router-dom";
 import Home from './components/pages/Home';
 import Services from "./components/pages/Services";
 import Products from "./components/pages/Products";
@@ -9,16 +9,16 @@ import SignUp from "./components/pages/SignUp";
 function App() {
   return (
     <>
-    <Router>
+    <HashRouter>
         <Navbar/>
         <Switch>
-            <Route path={process.env.PUBLIC_URL + '/'} exact component={Home}/>
-            <Route path={process.env.PUBLIC_URL + '/services'} component={Services}/>
-            <Route path={process.env.PUBLIC_URL + '/products'} component={Products}/>
-            <Route path={process.env.PUBLIC_URL + '/sign_up'} component={SignUp}/>
+            <Route path='/' exact component={Home}/>
+            <Route path='/services' component={Services}/>
+            <Route path='/products' component={Products}/>
+            <Route path='/sign_up' component={SignUp}/>
 
         </Switch>
-    </Router>
+    </HashRouter>
     </>
   );
 }
